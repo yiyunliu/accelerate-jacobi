eba2f239bfb548d6fb6745d6d93dcf985b5ad617
Yiyun Liu <liuyiyun@terpmail.umd.edu>
2019-06-08 22:54:10 -0400

    time to move on to cuda

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,17 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE ApplicativeDo #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE ViewPatterns #-}
-{-# LANGUAGE TemplateHaskell #-}
 
 module Main where
 
 import Control.Applicative (liftA2)
 import Data.Array.Accelerate as A
-import Data.Array.Accelerate.LLVM.Native as A
+import Data.Array.Accelerate.LLVM.Native as CPU
+import Criterion.Main
+import Control.DeepSeq
+
 
 -- | Dot product. stolen from accelerate's doc
 dotp :: A.Num a => Acc (Vector a) -> Acc (Vector a) -> Acc (Scalar a)
@@ -37,9 +38,9 @@ jacobiIter ::
 jacobiIter m d v x = A.zipWith (/) (A.zipWith (A.-) v (mvm m x)) d
 
 -- | Generate a hardwired RHS of the linear equation
-genVec :: Int -> Vector Double
+genVec :: Exp Int -> Acc (Vector Double)
 genVec dim =
-  fromFunction (Z :. dim) (const (Prelude.fromIntegral $ 3 Prelude.* dim - 1))
+  generate (index1 dim) (const (A.fromIntegral $ 3 A.* dim - 1))
 
 -- | Acc version of iterate
 aiterate ::
@@ -67,33 +68,41 @@ decompose m = lift (imap g m, d)
     g ((unlift . unindex2) -> (i, j)) e = A.cond (i A.== j) 0 e
 
 -- | Generate a hardwired diagonally dominant LHS of the linear equation.
-genMatrix' :: Int -> Matrix Double
-genMatrix' dim = fromFunction (Z :. dim :. dim) f
+genMatrix' :: Exp Int -> Acc (Matrix Double)
+genMatrix' dim = generate (index2 dim dim) f
   where
-    f (_ :. i :. j) =
-      let fi :: Double = Prelude.fromIntegral i
-       in if i Prelude.== j
-            then fi Prelude.+ Prelude.fromIntegral dim
-            else 1.0
+    f ((unlift . unindex2) -> (i,j)) =
+      let fi :: Exp Double = A.fromIntegral i
+       in A.cond (i A.== j)
+            (fi A.+ A.fromIntegral dim)
+            1.0
 
 -- | Pack the LHS and RHS together so their dimensions always match
-genMatrixVec' :: Int -> (Matrix Double, Vector Double)
-genMatrixVec' = liftA2 (,) genMatrix' genVec
+genMatrixVec' :: Exp Int -> Acc (Matrix Double, Vector Double)
+genMatrixVec' = lift . liftA2 (,) genMatrix' genVec
+
+genTest :: Exp Int -> Exp Int -> Acc (Vector Double, Scalar Double)
+genTest k dim =
+  let (m, ubvec) = unlift $ genMatrixVec' dim
+      (r, d) = unlift . decompose $ m
+      computation =
+        aiterate (A.unit $ A.lift k) (jacobiIter r d ubvec) (fill (shape d) 0)
+      error = error2 (mvm m computation) ubvec in
+    A.lift (computation, error)
+
+-- main :: IO ()
+-- main = do
+--   k :: Int <- readLn
+--   -- read the number d, where the dimension dim = 2^d
+--   dim :: Int <- (2 Prelude.^) <$> readLn
+--   -- construct the computation and send everything to the CPU/GPU
+--   print . CPU.run $ genTest 100 (2 Prelude.^ 14)
 
 main :: IO ()
 main
-  -- read the number of iterations
- = do
-  k :: Int <- readLn
-  -- read the number d, where the dimension dim = 2^d
-  dim :: Int <- (2 Prelude.^) <$> readLn
-  let (matrix, bvec) = genMatrixVec' dim
-  -- construct the computation before sending everything to the CPU/GPU
-  let m = use matrix
-      (r, d) = unlift . decompose $ m
-      ubvec = use bvec
-      computation =
-        aiterate (A.unit $ A.lift k) (jacobiIter r d ubvec) (fill (shape d) 0)
-      error = error2 (mvm m computation) ubvec
-  -- run the program and print out the result as a tuple containing the solution and error
-  print $ A.run (lift (computation, error))
+ = defaultMain
+  [ bench "16 Iterations, 2^12 Dimensions" $ nf CPU.run (genTest 16 (2 Prelude.^ 12))
+  , bench "100 Iterations, 2^12 Dimensions" $ nf CPU.run (genTest 100 (2 Prelude.^ 12))
+  , bench "16 Iterations, 2^14 Dimensions" $ nf CPU.run (genTest 16 (2 Prelude.^ 14))
+  ]
+
